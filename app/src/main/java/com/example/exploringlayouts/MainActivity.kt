b3358353cdb32a1a81b38e970751a4575b50ca3b
edwin001-tech <edwinshitakwa8@gmail.com>
2020-05-12 16:31:49 +0300

    initial commit

---
+++ app/src/main/java/com/example/exploringlayouts/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.exploringlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+        fun loadConstraintLayout(v: View) {
+            setContentView(R.layout.activity_main)
+        }
+
+        fun loadTableLayout(v: View) {
+          //  setContentView(R.layout.my_table_layout)
+        }
+
+    }
+}
